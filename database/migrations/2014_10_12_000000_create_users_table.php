a51be48367b4cf5e2824d8cdce96eab4eff40515
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2022-10-31 19:34:25 +0200

    init commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+         * USER TYPES *
+         * USR---> CUSTOMER
+         * ADM---> SUPER ADMIN
+         * WHS---> WAREHOUSE MANAGER
+         * DLV---> SHIPPING MANAGER
+         * DRV---> DRIVER
+         */
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('ID_NO');
+            $table->string('phone');
+            $table->string('user_type')->default('USR');
+            $table->string('image');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
